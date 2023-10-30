72021f314e8d9b4ae7e42f20b6f82e2553d9f40d
Shizuku_Kazuki <k021g1377@g.neec.ac.jp>
2023-10-31 01:40:37 +0900

    2-4

---
+++ TextureConverter.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+class TextureConverter
+{
+public:
+
+	//テクスチャをWICからDDSに変換する
+	void ConvertTextureWICToDDS(const std::string& filePath);
+
+private:
+	//テクスチャファイル読み込み
+	void LoadWICTextureFromFile(const std::string& filePath);
+private:
+	//マルチバイト文字列をワイド文字列に変換
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+};
+
--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include <cstdio>
 #include <cstdlib>
+#include<DirectXTex.h>
+#include "TextureConverter.h"
 
+//コマンドライン引数
+enum Argument
+{
+	kAppliCAtionPath, //アプリケーションのパス
+	kFilePath,		 //渡されたファイルのパス
+
+	NumArgument
+};
 int main(int argc,char* argv[])
 {
-	//argcの数だけ繰り返す
-	for (int i = 0; i < argc; i++)
-	{
-		//文字列argvのi番を表示
-		printf(argv[i]);
-		printf("\n");
-	}
+	////argcの数だけ繰り返す
+	//for (int i = 0; i < argc; i++)
+	//{
+	//	//文字列argvのi番を表示
+	//	printf(argv[i]);
+	//	printf("\n");
+	//}
+
+	assert(argc >= NumArgument);
+
+	//COMライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+	//テクスチャコンバータ
+	TextureConverter converter;
+	//テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+	//COMライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 	return 0;
